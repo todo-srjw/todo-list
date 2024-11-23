cf6fbcbd5001197460fe0f8dc933e3fad374a6a2
hyunsuk1111 <epsxkf@gmail.com>
2024-11-23 11:44:16 +0900

    FEAT : 멤버 엔티티 객체 생성
    멤버 및 base 엔티티 객체 구현

---
+++ src/main/java/com/example/todolist/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.todolist.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date", nullable = false)
+    private LocalDateTime updatedDate;
+
+}
