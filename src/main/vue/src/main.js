156f4c3cd5f2a6616ab759fc38c4c509416340a6
kimsoryeon <kimsr9210@naver.com>
2024-11-27 04:54:02 +0900

    vue 설정

--- src/main/vue/src/main.js
+++ src/main/vue/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+// Axios 기본 설정
+axios.defaults.baseURL = 'http://localhost:8081'; // Spring Boot 서버 URL
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+createApp(App)
+    .provide('axios', axios)
+    .use(router)
+    .mount('#app')
