354b60ab71d886e5aca530a0814aefc9618c3826
kimsoryeon <kimsr9210@naver.com>
2024-11-27 04:56:08 +0900

    Revert "vue 설정"
    This reverts commit 156f4c3cd5f2a6616ab759fc38c4c509416340a6.

--- src/main/vue/src/App.vue
+++ src/main/vue/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <PostPage/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue';
-import PostPage from './views/Post.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
-    PostPage
+    HelloWorld
   }
 }
 </script>
--- src/main/vue/src/main.js
+++ src/main/vue/src/main.js
@@ -1,14 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
 
-// Axios 기본 설정
-axios.defaults.baseURL = 'http://localhost:8081'; // Spring Boot 서버 URL
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-
-createApp(App)
-    .provide('axios', axios)
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
