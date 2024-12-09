b8e44756ed27cf0b5bc13b868ffeaa5e2fd81cd1
kimsoryeon <kimsr9210@naver.com>
2024-12-09 20:14:43 +0900

    FEAT : 알림 뼈대 생성

--- src/main/java/com/example/todolist/notification/repositrory/NotificationRepository.java
+++ src/main/java/com/example/todolist/notification/repositrory/NotificationRepository.java
@@ -1,8 +1,4 @@
 package com.example.todolist.notification.repositrory;
 
-
-import com.example.todolist.notification.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository {
 }
